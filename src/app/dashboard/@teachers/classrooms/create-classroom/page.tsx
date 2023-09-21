71111312c0f3b28e649e30489022b56076efe4d7
TharinduDeshananda <tdeshananda@gmail.com>
2023-09-21 11:29:16 +0530

    formik changes

--- src/app/dashboard/@teachers/classrooms/create-classroom/page.tsx
+++ src/app/dashboard/@teachers/classrooms/create-classroom/page.tsx
@@ -59,92 +59,117 @@ function CreateRoomPage() {
 
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <div className="p-1 sm:p-2 md:p-5 gap-x-2 gap-y-3 content-start generic-padding w-full max-w-4xl bg-white min-h-[400px] mx-auto mt-5 rounded-md shadow-md grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        <h1 className="col-span-1 mb-5 text-base font-bold sm:col-span-2 md:col-span-3 sm:text-lg md:text-xl">
-          Enter Class Details
-        </h1>
-        {(gradeQuery.isLoading || subjectQuery.isLoading) && (
-          <div className="col-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center gap-x-2">
-            <div className="animate-spin rounded-full w-6 h-6 border-2 border-transparent border-b-blue-600 border-l-blue-600 "></div>
-            <div className="text-xs text-gray-500">please wait</div>
-          </div>
-        )}
-        {(gradeQuery.isError || subjectQuery.isError) && (
-          <div className="col-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center gap-x-2">
-            <div className="text-xs text-red-500 border border-red-500 p-5">
-              Something went wrong.
+      <form onSubmit={formik.handleSubmit}>
+        <div className="p-1 sm:p-2 md:p-5 gap-x-2 gap-y-3 content-start generic-padding w-full max-w-4xl bg-white min-h-[400px] mx-auto mt-5 rounded-md shadow-md grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <h1 className="col-span-1 mb-5 text-base font-bold sm:col-span-2 md:col-span-3 sm:text-lg md:text-xl">
+            Enter Class Details
+          </h1>
+          {(gradeQuery.isLoading || subjectQuery.isLoading) && (
+            <div className="col-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center gap-x-2">
+              <div className="animate-spin rounded-full w-6 h-6 border-2 border-transparent border-b-blue-600 border-l-blue-600 "></div>
+              <div className="text-xs text-gray-500">please wait</div>
             </div>
-          </div>
-        )}
+          )}
+          {(gradeQuery.isError || subjectQuery.isError) && (
+            <div className="col-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center gap-x-2">
+              <div className="text-xs text-red-500 border border-red-500 p-5">
+                Something went wrong.
+              </div>
+            </div>
+          )}
 
-        {/* class name */}
-        <CustomInputField
-          placeholder="class name"
-          type="text"
-          inputName="className"
-          onChangeHandle={formik.handleChange}
-          value={formik.values.className}
-        />
-        {/* class code */}
-        <CustomInputField
-          placeholder="class code"
-          type="text"
-          inputName="classCode"
-          onChangeHandle={formik.handleChange}
-          value={formik.values.classCode}
-        />
-        {/* description */}
-        <div className="col-span-1 col-start-1 sm:col-span-2 md:col-span-3">
-          <CustomTextArea
-            placeholder="class description"
-            cols={5}
-            inputName="description"
+          {/* class name */}
+          <CustomInputField
+            placeholder="class name"
+            type="text"
+            inputName="className"
             onChangeHandle={formik.handleChange}
-            value={formik.values.description}
+            value={formik.values.className}
           />
-        </div>
-        <div className="">
-          <CustomSelectField
-            placeholder="grade"
-            options={(gradeQuery.isSuccess && gradeQuery.data
-              ? gradeQuery.data
-              : []
-            ).map(
-              (i) =>
-                ({ optionName: i.gradeName, value: i.gradeCode } as SelectType)
-            )}
+          {/* class code */}
+          <CustomInputField
+            placeholder="class code"
+            type="text"
+            inputName="classCode"
+            onChangeHandle={formik.handleChange}
+            value={formik.values.classCode}
           />
+          {/* description */}
+          <div className="col-span-1 col-start-1 sm:col-span-2 md:col-span-3">
+            <CustomTextArea
+              placeholder="class description"
+              cols={5}
+              inputName="description"
+              onChangeHandle={formik.handleChange}
+              value={formik.values.description}
+            />
+          </div>
+          <div className="">
+            <CustomSelectField
+              placeholder="grade"
+              inputName="grade"
+              value={formik.values.grade}
+              onBlurHandle={formik.handleBlur}
+              onChangeHandle={formik.handleChange}
+              options={(gradeQuery.isSuccess && gradeQuery.data
+                ? gradeQuery.data
+                : []
+              ).map(
+                (i) =>
+                  ({
+                    optionName: i.gradeName,
+                    value: i.gradeCode,
+                  } as SelectType)
+              )}
+            />
+          </div>
+          <div className="">
+            <CustomSelectField
+              placeholder="subject"
+              inputName="subject"
+              value={formik.values.subject}
+              onBlurHandle={formik.handleBlur}
+              onChangeHandle={formik.handleChange}
+              options={(subjectQuery.isSuccess && subjectQuery.data
+                ? subjectQuery.data
+                : []
+              ).map(
+                (i) =>
+                  ({
+                    optionName: i.subjectName,
+                    value: i.subjectCode,
+                  } as SelectType)
+              )}
+            />
+          </div>
+          <div className="">
+            <CustomInputField
+              type="number"
+              placeholder="class year"
+              inputName="year"
+              onChangeHandle={formik.handleChange}
+              value={formik.values.year.toString()}
+            />
+          </div>
+          <div className="col-span-1 sm:col-span-2 md:col-span-3">
+            <CustomTextArea
+              placeholder="class time description"
+              cols={5}
+              inputName="timeString"
+              onChangeHandle={formik.handleChange}
+              value={formik.values.timeString}
+            />
+          </div>
+          <div className="col-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center py-5">
+            <input
+              type="submit"
+              value={"Create Class"}
+              className="generic-button-primary disabled:bg-gray-600"
+              disabled={!gradeQuery.isSuccess || !subjectQuery.isSuccess}
+            />
+          </div>
         </div>
-        <div className="">
-          <CustomSelectField
-            placeholder="subject"
-            options={(subjectQuery.isSuccess && subjectQuery.data
-              ? subjectQuery.data
-              : []
-            ).map(
-              (i) =>
-                ({
-                  optionName: i.subjectName,
-                  value: i.subjectCode,
-                } as SelectType)
-            )}
-          />
-        </div>
-        <div className="">
-          <CustomInputField type="number" placeholder="class year" />
-        </div>
-        <div className="col-span-1 sm:col-span-2 md:col-span-3">
-          <CustomInputField type="text" placeholder="class time description" />
-        </div>
-        <div className="col-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center py-5">
-          <input
-            type="submit"
-            value={"Create Class"}
-            className="generic-button-primary disabled:bg-gray-600"
-            disabled={!gradeQuery.isSuccess || !subjectQuery.isSuccess}
-          />
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
