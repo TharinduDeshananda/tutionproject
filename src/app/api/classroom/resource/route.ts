fb8bda0788a6a927bc8c14aead445873db0324d2
TharinduDeshananda <tdeshananda@gmail.com>
2023-10-10 14:13:29 +0530

    changes

--- src/app/api/classroom/resource/route.ts
+++ src/app/api/classroom/resource/route.ts
@@ -1,16 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import multer from "multer";
-import { NextApiRequest, NextApiResponse } from "next";
+
 const upload = multer().single("resourceFiles");
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest) {
   try {
-    await new Promise((resolve, reject) => {
-      upload(req, res, (err) => {
-        if (err) reject(err);
-        resolve("OK");
-      });
-    });
+    return NextResponse.json("OK");
   } catch (e) {
     console.log("Resource upload failed: ", e);
     NextResponse.json({ status: 1, message: e.message, body: e });
