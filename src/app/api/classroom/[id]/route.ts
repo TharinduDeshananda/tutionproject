6c6e2808a150030aa14d1cf1afdbf19a44fbebf5
TharinduDeshananda <tdeshananda@gmail.com>
2024-01-27 18:14:00 +0530

    class room access restrictions added

--- src/app/api/classroom/[id]/route.ts
+++ src/app/api/classroom/[id]/route.ts
@@ -1,7 +1,12 @@
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 import ClassRoomDto from "src/models/dto/ClassRoomDto";
 import { getClassRoomById } from "src/services/ClassRoomService";
+import { authOptions } from "../../auth/[...nextauth]/route";
+import UserRole from "src/enum/UserRole";
+import { db } from "src/helpers/db";
+import mongoose from "mongoose";
 
 export async function GET(
   request: NextRequest,
@@ -9,6 +14,35 @@ export async function GET(
 ) {
   try {
     if (!params.id) throw new Error("Class room id is required.");
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id;
+    const userRole = session?.user?.role;
+    if (!userId || !userRole) {
+      console.log("user role or id is empty");
+      throw new Error("unauthorized");
+    }
+
+    //check role based access
+    if (userRole === UserRole.STUDENT.toString()) {
+      const exists = await db.ClassRoomEntity.exists({
+        _id: new mongoose.Types.ObjectId(params.id),
+        students: { $elemMatch: { $eq: new mongoose.Types.ObjectId(userId) } },
+      });
+      if (!exists) {
+        console.log("student does not exist in class");
+        throw new Error("unauthorized");
+      }
+    } else {
+      const exists = await db.ClassRoomEntity.exists({
+        _id: new mongoose.Types.ObjectId(params.id),
+        teacher: new mongoose.Types.ObjectId(userId),
+      });
+      if (!exists) {
+        console.log("teacher do not own the class");
+        throw new Error("unauthorized");
+      }
+    }
+
     const classRoom: ClassRoomDto | null = await getClassRoomById(params.id);
     return NextResponse.json({
       status: 0,
--- src/queries/classroom/ClassRoomQueries.ts
+++ src/queries/classroom/ClassRoomQueries.ts
@@ -1,3 +1,5 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "src/app/api/auth/[...nextauth]/route";
 import { getClassRoomById } from "src/services/ClassRoomService";
 
 export async function getClassRoomByClassRoomIdQuery(id: string) {
